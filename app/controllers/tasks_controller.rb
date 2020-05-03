7df7b7d3ea1dde0d88e93993fd91eb6b5f117791
hide.naga <hh1120@hotmail.co.jp>
2020-05-03 16:09:06 +0000

    amend controller

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,5 +1,7 @@
 class TasksController < ApplicationController
   before_action :set_task, only: [:show, :edit, :update, :destroy]
+  before_action :correct_user, only: [:show]
+  
   def index
     if logged_in?
       @task = current_user.tasks.build  # form_with 用
